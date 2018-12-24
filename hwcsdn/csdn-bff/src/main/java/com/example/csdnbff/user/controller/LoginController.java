3b452fd750b79a422fc339d8ac5beea3c4c99d1e
hewei <hw123520mm@163.com>
2018-12-24 16:27:58 +0800

    登录完成

---
+++ hwcsdn/csdn-bff/src/main/java/com/example/csdnbff/user/controller/LoginController.java
@@ -0,0 +1,35 @@
+package com.example.csdnbff.user.controller;
+
+import com.example.common.user.UserInfo;
+import com.example.common.util.JsonUtil;
+import com.example.common.util.Message;
+import com.example.csdnbff.user.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+@Controller
+@RequestMapping("/userLogin")
+public class LoginController {
+    @Autowired
+    UserService userService;
+    @RequestMapping(value = "/tologin", method = RequestMethod.POST)
+    public String tologin(@RequestBody String requestBody, HttpServletRequest request){
+        try{
+            Message msg = userService.userLogin(requestBody);
+            if("0000".equals(msg.getSuccess())){
+                HttpSession session=request.getSession();//获取session并将userName存入session对象
+                UserInfo userInfo= JsonUtil.toBean(JsonUtil.convertObjectToJSON(msg.getData()),UserInfo.class);
+                session.setAttribute("userInfo", userInfo);
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return "index";
+    }
+}
--- hwcsdn/csdn-bff/src/main/java/com/example/csdnbff/user/controller/UserController.java
+++ hwcsdn/csdn-bff/src/main/java/com/example/csdnbff/user/controller/UserController.java
@@ -1,12 +1,16 @@
 package com.example.csdnbff.user.controller;
 
+import com.example.common.user.UserInfo;
+import com.example.common.util.JsonUtil;
 import com.example.common.util.Message;
 import com.example.csdnbff.user.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 @RestController
 @RequestMapping("/userInfo")
@@ -16,11 +20,16 @@ public class UserController {
     UserService userService;
 
     @RequestMapping(value = "/userLogin", method = RequestMethod.POST)
-    public Message userLogin(@RequestBody String requestBody) {
+    public Message userLogin(@RequestBody String requestBody, HttpServletRequest request) {
         Message msg = new Message();
         try {
             System.out.println(requestBody);
             msg = userService.userLogin(requestBody);
+            if ("0000".equals(msg.getSuccess())){
+                HttpSession session=request.getSession();
+                UserInfo userInfo= JsonUtil.toBean(JsonUtil.convertObjectToJSON(msg.getData()),UserInfo.class);
+                session.setAttribute("userInfo",userInfo);
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -37,5 +46,15 @@ public class UserController {
             e.printStackTrace();
         }
         return msg;
+    }
+    @RequestMapping(value = "/tologin", method = RequestMethod.GET)
+    public ModelAndView tologin() {
+        ModelAndView mv=new ModelAndView("user/tologin");
+        return mv;
+    }
+    @RequestMapping(value = "/toindex", method = RequestMethod.GET)
+    public ModelAndView toindex() {
+        ModelAndView mv=new ModelAndView("index");
+        return mv;
     }
 }
